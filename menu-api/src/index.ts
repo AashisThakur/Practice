b85890ec6356b53118b38c0a12544f15fe941af3
aath-odoo <aath@odoo.com>
2023-05-23 10:38:30 +0530

    menu api

---
+++ menu-api/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+
+dotenv.config();
+
+if(!process.env.PORT){
+    console.log("ssss")
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/", itemsRouter);
+
+
+app.listen(PORT , () => {
+    console.log(`"listening at : ",${PORT}`);
+});
---
+++ menu-api/src/items/items.service.ts
@@ -0,0 +1,73 @@
+
+/**
+ * Data Model Interfaces
+ */
+import { BaseItems,Item } from "./item.interface";
+import { Items } from "./items.interface";
+
+let items : Items = {
+   1 : { 
+    id: 1,
+    name: "Burger",
+    price: 37,
+    description: "ACHA HAI",
+    image: "baaadme daalunga",
+   },
+   2 : { 
+    id: 2,
+    name: "Pizza",
+    price: 120,
+    description: "Dominoz",
+    image: "baaadme daalunga",
+   },
+   3 : { 
+    id: 3,
+    name: "Sandwich",
+    price: 70,
+    description: "Grilled cheese",
+    image: "baaadme daalunga",
+   },
+};
+
+
+/**
+ * Service Methods
+ */
+export const findAll = async (): Promise<Item[]> => Object.values(items);
+export const find = async (id: number): Promise<Item> => items[id];
+
+
+export const create = async (newItem: BaseItems): Promise<Item> => {
+ const id = new Date().valueOf();
+
+
+ items[id] = {
+    id,
+    ...newItem,
+ }
+
+ return items[id];
+
+}
+
+export const update = async ( id: number, itemUpdate: BaseItems ): Promise<Item|null> => {
+    const item = await find(id);
+
+    if(!item){
+        return null;
+    }
+
+    items[id]= {id, ...itemUpdate};
+
+    return items[id];
+}
+
+export const remove = async (id: number): Promise<null | void> => {
+    const item = await find(id);
+  
+    if (!item) {
+      return null;
+    }
+  
+    delete items[id];
+  };
